2e1cd516aed7b171e900aaa2180840ffb16f5ca5
Son <sonqb2001567@gmail.com>
2025-04-12 16:21:50 +0700

    create picture collumn in template

--- Formcv.php
+++ Formcv.php
@@ -35,23 +35,7 @@
 </head>
 
 <body class="bg-light d-flex justify-content-center position-relative" > <!--back-ground-->
-    <div id="position-fixed template-holder-container" class="position-absolute top-0 start-0">
-        <div class="btn position-fixed" onclick="tempPlateButtonClick()">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-file-earmark-person-fill" viewBox="0 0 16 16">
-                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0m2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755"/>
-            </svg>
-        </div>
-
-        <div id="templates-holder" class="position-fixed top-0 start-0" style="display: none;" onclick="tempPlateCloseButtonClick(this)">
-            <div class="btn position-fixed">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left" style="color: white;" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
-                </svg>
-            </div>
-            
-        </div>
-    </div>
-    
+    <?php include('templateHolder.php')?>
     <div id="cv-form" class="mt-4 mb-4 bg-white d-flex flex-row"> <!--cv-form-->
         <div class="bg-secondary text-white w-50">  <!--Additional infor-->
             <div id="cv-element" class="mt-2">
