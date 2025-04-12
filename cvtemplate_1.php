6450e9727781ef4ce83e2f8713e02ca9fe4afd1b
Son <sonqb2001567@gmail.com>
2025-04-12 20:27:42 +0700

    update contact infor in cv_content table

--- cvtemplate_1.php
+++ cvtemplate_1.php
@@ -6,6 +6,8 @@
     if (empty($cvContent)) {
         die("query Error: " . $conn->connect_error);
     }
+
+
 ?>
 
 <div id="cv-form" class="mt-4 mb-4 bg-white d-flex flex-row"> <!--cv-form-->
@@ -141,7 +143,7 @@
     </div>
     <div class="text-black w-100 d-flex flex-column"> <!--main infor-->
         <div id="cv-element" class="fs-4">
-            <strong contenteditable="true"><?php echo htmlspecialchars( $cvContent["name"]); ?></strong>
+            <strong contenteditable="true"><?php echo htmlspecialchars( $cvContent["name"]);?></strong>
         </div>
         <div id="cv-element" class="fs-4">
             <strong contenteditable="true">Developer Fresher</strong>
