f37ff3ecaa04b2fb3168e0325acd0ca46ced7a24
Sang <sang.doanbk1@hcmut.edu.vn>
2025-04-13 11:24:31 +0700

    add connection.php to connect to DB and submitionForm for getting cv_content, pagination

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
+    include("connection.php");
+
+
     #$page = $_GET['page']
-    $page = isset($_GET['page']) ? $_GET['page'] : 'login';
+    $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
     $allowedPages = ['login', 'home', 'Formcv', 'product'];
+
+
+    $starter = 0;
+    $skip = 2; // number item per page
+
+    $records = $conn->query("SELECT * FROM template");
+    $total_rows = $records->num_rows;
+
+    // calculate number of pages, ceil for round up
+    $total_pages = ceil($total_rows/$skip);
+
+    // get the current page number
+    $page_number = 1;
+    if (isset($_GET['page_number'])) {
+        $page_number=$_GET['page_number'];
+        $starter=($page_number-1)*$skip;
+    }
+
 
     if (in_array($page, $allowedPages)) {
         if ($page == 'login'){
--- login.js
+++ login.js
@@ -1,27 +1,112 @@
-const accountInput = document.getElementById('account');
-const passwordInput = document.getElementById('password');
-const loginButton = document.querySelector("button[type='submit']");
 
-// Thêm sự kiện click để xử lý khi người dùng nhấn nút
-loginButton.addEventListener("click", function (e) {
-    // Ngăn chặn hành động mặc định của form (submit làm reload trang)
-    e.preventDefault();
-
-    // Lấy giá trị người dùng nhập
-    const username = accountInput.value.trim();
-    const password = passwordInput.value.trim();
-
-    if (!username || !password) {
-        alert("Please fill in both username and password.");
-        return;
-    }
-
-    // Kiểm tra thông tin đăng nhập
-    if (username === "1" && password === "1") {
-        // Nếu đúng, chuyển hướng đến trang home.html với tham số truy vấn
-        window.location.href = `http://localhost:8080/Web-programming/?page=home`;
-    } else {
-        // Nếu sai, hiển thị thông báo lỗi
-        alert("Invalid username or password. Please try again.");
-    }
-});
+    
+    <!-- contact_info -->
+    <div class="mt-4">
+      <h3>Contact Info (Optional):</h3>
+        <textarea class="tinymce" id="cv_content_contact_info" name="cv_content_contact_info" placeholder="Enter your contact information here.">
+          <?php
+            if (isset($cv_content_contact_info)){
+              echo $cv_content_contact_info;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- introduction -->
+    <div class="mt-4">
+      <h3>Introduction:</h3>
+        <textarea class="tinymce" id="cv_content_introduction" name="cv_content_introduction" placeholder="Enter your introduction here.">
+          <?php
+            if (isset($cv_content_introduction)){
+              echo $cv_content_introduction;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- career_goal NULL -->
+    <div class="mt-4">
+      <h3>Career Goal (Optional):</h3>
+        <textarea class="tinymce" id="cv_content_career_goal" name="cv_content_career_goal" placeholder="Enter your career goal here.">
+          <?php
+            if (isset($cv_content_career_goal)){
+              echo $cv_content_career_goal;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- experience -->
+    <div class="mt-4">
+      <h3>Experience:</h3>
+        <textarea class="tinymce" id="cv_content_experience" name="cv_content_experience" placeholder="Enter your experience here.">
+          <?php
+            if (isset($cv_content_experience)){
+              echo $cv_content_experience;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- education -->
+    <div class="mt-4">
+      <h3>Education:</h3>
+        <textarea class="tinymce" id="cv_content_education" name="cv_content_education" placeholder="Enter your education here.">
+          <?php
+            if (isset($cv_content_education)){
+              echo $cv_content_education;
+            }
+          ?>  
+        </textarea>
+    </div>
+    <!-- skills -->
+    <div class="mt-4">
+      <h3>Skills:</h3>
+        <textarea class="tinymce" id="cv_content_skills" name="cv_content_skills" placeholder="Enter your skills here.">
+          <?php
+            if (isset($cv_content_skills)){
+              echo $cv_content_skills;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- certificates -->
+    <div class="mt-4">
+      <h3>Certificates:</h3>
+        <textarea class="tinymce" id="cv_content_certificates" name="cv_content_certificates" placeholder="Enter your certificates here.">
+          <?php
+            if (isset($cv_content_certificates)){
+              echo $cv_content_certificates;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- awards -->
+    <div class="mt-4">
+      <h3>Awards:</h3>
+        <textarea class="tinymce" id="cv_content_awards" name="cv_content_awards" placeholder="Enter your awards here.">
+          <?php
+            if (isset($cv_content_awards)){
+              echo $cv_content_awards;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- additional_info NULL -->
+    <div class="mt-4">
+      <h3>Additional Info (Optional):</h3>
+        <textarea class="tinymce" id="cv_content_additional_info" name="cv_content_additional_info" placeholder="Enter your additional info here.">
+          <?php
+            if (isset($cv_content_additional_info)){
+              echo $cv_content_additional_info;
+            }
+          ?>
+        </textarea>
+    </div>
+    <!-- reference_person -->
+    <div class="mt-4">
+      <h3>Reference Person:</h3>
+        <textarea class="tinymce" id="cv_content_reference_person" name="cv_content_reference_person" placeholder="Enter your reference person here.">
+          <?php
+            if (isset($cv_content_reference_person)){
+              echo $cv_content_reference_person;
+            }
+          ?>
+        </textarea>
+    </div>
